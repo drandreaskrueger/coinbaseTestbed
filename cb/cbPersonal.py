a5e26958eccb27b2a980bc0efcecf4e12c5bf101
drandreaskrueger <study@andreaskrueger.de>
2016-01-13 06:22:33 +0100

    cbPersonal_ME for non-uploaded file with secret credentials

--- cb/cbPersonal.py
+++ cb/cbPersonal.py
@@ -5,6 +5,9 @@ Created on 10 Jan 2016
 
 @summary: API keys & secrets & emailaddress2 (to request money from)
 
+@attention: 
+https://developers.coinbase.com/docs/wallet/api-key-authentication#security-best-practices
+
 '''
 
 # fill in your credentials:
@@ -12,7 +15,14 @@ Created on 10 Jan 2016
 API_KEY=""
 API_SECRET=""
 
-# N.B.: Needs email address of a 2nd coinbase 'sign up' account, 
-# to request money from. (I cannot 'request_money' from myself.)
-
+# Email address of a 2nd coinbase 'sign up' account, to request money from. 
+# Cause I cannot 'request_money' from myself.
 EMAILADDRESS2=""
+
+
+try:
+  from cbPersonal_ME import API_KEY, API_SECRET, EMAILADDRESS, EMAILADDRESS2
+except:
+  pass
+else: 
+  print "Credentials from 'cbPersonal_ME.py' which is in .gitignore. Still, please think about better security!" 
